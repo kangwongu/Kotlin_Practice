073a8379126d004d12ac069a9ff7ba668a80b379
kangwongu <kwg0527@naver.com>
2024-02-09 21:32:43 +0900

    QueryDSL 리팩토링

--- kotlin_spring/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ kotlin_spring/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -4,13 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.Optional
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
 
     // 코틀린은 언어 자체에서 '?'로 Null을 체크해주기 때문에, Optional이 필요없다
 //    fun findByName(name: String): Optional<User>
     fun findByName(name: String): User?
 
-    @Query("select distinct u from User u " +
-            "left join fetch u.userLoanHistories ulh")
-    fun findAllWithHistories(): List<User>
+//    @Query("select distinct u from User u " +
+//            "left join fetch u.userLoanHistories ulh")
+//    fun findAllWithHistories(): List<User>
 }
