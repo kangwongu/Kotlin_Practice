3c37697e020369311df85a539fac2f64463623fa
kangwongu <kwg0527@naver.com>
2024-03-25 22:30:36 +0900

    Initial commit coroutine

---
+++ coroutine/src/main/kotlin/com/coroutine/Main.kt
@@ -0,0 +1,26 @@
+package com.coroutine
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
+fun main(): Unit = runBlocking {
+    printWithThread("START")
+    launch {
+        newRoutine()
+    }
+    // 양보
+    yield()
+    printWithThread("END")
+}
+
+suspend fun newRoutine() {
+    val num1 = 1
+    val num2 = 2
+    yield()
+    printWithThread("${num1 + num2}")
+}
+
+fun printWithThread(str: Any) {
+    println("[${Thread.currentThread().name}] $str")
+}
